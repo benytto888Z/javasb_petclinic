d165f2ef338ea0b3318ba62dec6c240de8065995
benytto888Z <benamos111@gmail.com>
2020-11-24 23:39:43 +0100

    Initial Project Add

---
+++ src/main/java/fr/creamind/petclinic/javasb_petclinic/JavasbPetclinicApplication.java
@@ -0,0 +1,13 @@
+package fr.creamind.petclinic.javasb_petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavasbPetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavasbPetclinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/creamind/petclinic/javasb_petclinic/JavasbPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.creamind.petclinic.javasb_petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavasbPetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
